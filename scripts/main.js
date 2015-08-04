20a10cf8c081e49e93453f0718c0813927a0ed63
Oyvind Hoie <hoie@draft.no>
2015-08-04 12:56:51 +0200

    Added animate.css and added wow.js with browserify shim Added animations to page

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 'use strict'
 
-window.$ = window.jQuery = require('jquery')
-var easing = require('jquery.easing')
-var bootstrap = require('bootstrap-sass')
+window.$ = window.jQuery = require('jquery');
+var easing = require('jquery.easing');
+var bootstrap = require('bootstrap-sass');
+var WOW = require('wow');
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 
 $('document').ready (function() {
+    
+    new WOW().init();
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -15,7 +19,7 @@ $('document').ready (function() {
         event.preventDefault();
     });
 
-    if ($(window).width() > '1024') {
-        $(window).stellar();
-    }; 
+    // if ($(window).width() > '1024') {
+    //     $(window).stellar();
+    // }; 
 });
