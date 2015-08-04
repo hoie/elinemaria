6593f6fa6f982b08dea0bb34486b98957fd95e68
Oyvind Hoie <hoie@draft.no>
2015-08-04 18:43:13 +0200

    Closes responsive menu when item clicked

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,23 @@ var easing = require('jquery.easing');
 var bootstrap = require('bootstrap-sass');
 var WOW = require('wow');
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
 
 $('document').ready (function() {
     
     new WOW().init();
 
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeOutExpo');
         event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
     });
 
     // if ($(window).width() > '1024') {
